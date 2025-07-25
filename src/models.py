894c843b26b1c6d9073e1b02c8f653bb5e7a51da
Jose <jm.bebor@gmail.com>
2025-07-25 11:04:05 +0000

    [WIP]

--- src/models.py
+++ src/models.py
@@ -1,14 +1,37 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from typing import List
 
 db = SQLAlchemy()
+
+# Usuarios
+# personajes
+# planetas
+# favoritos
+
+# relaciones
 
 class User(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
+
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "email": self.email,
+            # do not serialize the password, its a security breach
+        }
+
+class Character(db.Model):
+    character_id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    gender: Mapped[str] = mapped_column(nullable=False)
+    hair_color: Mapped[str] = mapped_column(nullable=False)
+    eye_color: Mapped[str] = mapped_column(nullable=False)
 
 
     def serialize(self):
